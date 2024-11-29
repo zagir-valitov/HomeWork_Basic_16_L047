5981ee03864bf249a7544deba584a2e9b4317c29
Загир Валитов <zagir-valitov@outlook.com>
2024-11-29 21:45:46 +0500

    Удалил ненужные модели

--- LinqToDB/Models/ActorModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DapperToDB.Models;
-
-public class ActorModel
-{
-    public int Actor_ID { get; set; }
-    public string? First_Name { get; set; }
-    public string? Last_Name { get; set; }
-    public string? Last_Update { get; set; }
-}
